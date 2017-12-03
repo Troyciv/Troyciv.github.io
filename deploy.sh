43dce8654b3bbda02306d429a2f6bcaa5edca10b
pcuinty <ciusubv.g6@dispostable.com>
2017-12-03 19:50:40 +0100

    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+git add --all && \
+git add -u && \
+git status && \
+read -p 'Enter the commit message:' commitMessage
+git commit -m "$commitMessage"
+# git push origin sources
+# read
+
+# git -C _site/ add --all
+# git -C _site/ status
+# git -C _site/ commit
+# git -C _site/ push origin master
